aeff90948061fdd3d6dcb55708934ae5b6769fec
HeadMad <kov.ps@ya.ru>
2025-06-22 19:00:20 +0800

    fix

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,11 @@
+import { build } from '$service-worker';
 import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
 		adapter: adapter({
+			pages: 'build',
 			fallback: '404.html'
 		}),
 		paths: {
