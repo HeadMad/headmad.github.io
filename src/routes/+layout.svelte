5aff8377f2be39d19165f736836d56d847a651c4
HeadMad <kov.ps@ya.ru>
2025-06-22 20:23:55 +0800

    add some

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,8 +5,9 @@
 
 <nav>
   <a href="/">Home</a> |
-  <a href="/faq">FAQ</a>|
-  <a href="/contacts">Contacts</a>
+  <a href="/faq">FAQ</a> |
+  <a href="/contacts">Contacts</a> |
+  <a href="/some">Some</a>
 </nav>
 
 {@render children()}
