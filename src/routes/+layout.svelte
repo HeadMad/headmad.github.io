2789e99c5ceb372b25723c942dd7970d72e97087
HeadMad <kov.ps@ya.ru>
2025-06-22 19:32:30 +0800

    change base paths in layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,12 @@
 <script>
+  import {base} from '$app/paths';
    let {children} = $props();
 </script>
 
 <nav>
   <a href="/">Home</a> |
-  <a href="/faq">FAQ</a>|
-  <a href="/contacts">Contacts</a>
+  <a href="{base}/faq">FAQ</a>|
+  <a href="{base}/contacts">Contacts</a>
 </nav>
 
 {@render children()}
