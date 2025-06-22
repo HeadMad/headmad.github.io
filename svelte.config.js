17b7869b8f60139ecd378db5aa3024407c72157c
HeadMad <kov.ps@ya.ru>
2025-06-22 20:21:29 +0800

    fix config

--- svelte.config.js
+++ svelte.config.js
@@ -6,10 +6,7 @@ const config = {
 		adapter: adapter({
 			pages: 'build',
 			fallback: '404.html'
-		}),
-		paths: {
-			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
-		}
+		})
 	}
 };
 
