27506ae75cfec8882ce162f6dc6a5b8ba73ad709
headmad <kov.ps@ya.ru>
2025-11-30 01:10:46 +0800

    change index page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,11 @@
+<script>
+  let search = $state("");
+  $effect(() => {
+    search = Object.fromEntries(new URLSearchParams(window.location.search));
+  }); 
+</script>
 <h1>Home</h1>
+
+<pre>
+  {JSON.stringify(search, null, 2)}
+</pre>
